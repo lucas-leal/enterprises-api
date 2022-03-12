98f1af60a931b72a0155bb9294c064b8fbf53425
Lucas Leal <lucas.ity.leal@gmail.com>
2022-03-12 15:25:18 -0300

    feat: seed default user

--- src/models/sequelize.sync.js
+++ src/models/sequelize.sync.js
@@ -2,4 +2,13 @@ require('dotenv').config();
 
 const User = require('./user');
 
-User.sync({force: true});
+let synchronizeDatabase = async () => {
+    await User.sync({force: true});
+
+    User.create({
+        username: 'admin',
+        password: '$2b$04$W8MipPZhsljb.RAy/LRNu.JnrfK5mFUJb8Y.WiRajeL7lOnqY6qsi'
+    });
+}
+
+synchronizeDatabase();
