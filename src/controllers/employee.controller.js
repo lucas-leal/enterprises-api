e5e5cf35e5db662b1ce7b1a76f6303ab346f6732
Lucas Leal <lucas.ity.leal@gmail.com>
2022-03-12 16:59:09 -0300

    feat: include try/catch

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -10,29 +10,30 @@ router.get('/', async (req, res) => {
     res.send(employees);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
+    try {
+        const employee = await Employee.create({
+            name: req.body.name,
+            documentNumber: req.body.documentNumber,
+            birthDate: req.body.birthDate,
+            position: req.body.position,
+            startDate: req.body.startDate,
+            salary: req.body.salary
+        });
     
-    const employee = await Employee.create({
-        name: req.body.name,
-        documentNumber: req.body.documentNumber,
-        birthDate: req.body.birthDate,
-        position: req.body.position,
-        startDate: req.body.startDate,
-        salary: req.body.salary
-    });
-
-    const address = await Address.create({
-        addressLine: 'foo',
-        city: 'Belo Horizonte',
-        state: 'MG',
-        zipCode: '30890000',
-        country: 'Brazil',
-        EmployeeId: employee.id
-    });
-
-    employee.getAddress();
-
-    res.send(employee);
-})
+        const address = await Address.create({
+            addressLine: 'foo',
+            city: 'Belo Horizonte',
+            state: 'MG',
+            zipCode: '30890000',
+            country: 'Brazil',
+            EmployeeId: employee.id
+        });
+    
+        res.send(employee);
+    } catch (error) {
+        next(error);
+    }
+});
 
 module.exports = router;
