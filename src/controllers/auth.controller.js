7b7a0a29a5d0fc64f97ad29969df53db986b6684
Lucas Leal <lucas.ity.leal@gmail.com>
2022-03-12 15:15:05 -0300

    feat: find users on database for authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,15 +1,25 @@
-const jwt = require('jsonwebtoken')
-const UserError = require('../user.error')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-const DEFAULT_USERNAME = 'admin'
-const DEFAULT_PASSWORD = 'A234$6'
+const UserError = require('../user.error');
+const User = require('../models/user');
 
-module.exports.authorize = (req, res) => {
-    if (req.body.username !== DEFAULT_USERNAME || req.body.password !== DEFAULT_PASSWORD) {
-        throw new UserError('Wrong credentials')
+module.exports.authorize = async (req, res, next) => {
+    try {
+        const user = await User.findOne({where: {username: req.body.username}});
+
+        if (user instanceof User) {
+            if (bcrypt.compareSync(req.body.password, user.password)) {
+                const accessToken = jwt.sign({}, process.env.PRIVATE_KEY, {expiresIn: 3600});
+
+                res.send({accessToken});
+                
+                return;
+            }
+        }
+
+        throw new UserError('Wrong credentials');
+    } catch (error) {
+        next(error);
     }
-
-    let accessToken = jwt.sign({}, process.env.PRIVATE_KEY, {expiresIn: 3600})
-
-    res.send({accessToken})
 }
