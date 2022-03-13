e40457245e5c612f6ae779a4996e90dda537d552
Lucas Leal <lucas.ity.leal@gmail.com>
2022-03-13 20:55:57 -0300

    feat: move user creation code to inside try/catch

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,17 +1,22 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
-module.exports.create = async (req, res) => {
-    let salt = Math.random();
-    let hash = bcrypt.hashSync(req.body.password, salt)
-    
-    const user = await User.create({
-        username: req.body.username,
-        password: hash
-    });
+module.exports.create = async (req, res, next) => {
+    try {
+        let salt = Math.random();
+        let hash = bcrypt.hashSync(req.body.password, salt)
+        
+        const user = await User.create({
+            username: req.body.username,
+            password: hash,
+            employeeId: req.body.employeeId
+        });
 
-    res.send({
-        id: user.id, 
-        username: user.username
-    });
+        res.send({
+            id: user.id, 
+            username: user.username
+        });
+    } catch (error) {
+        next(error);
+    }
 }
