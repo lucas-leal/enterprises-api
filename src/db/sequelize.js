d5f4814d5df69e850acc0468f112bac4548b2c07
Lucas Leal <lucas.ity.leal@gmail.com>
2022-03-14 00:51:12 -0300

    feat: add database port in env file

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -2,5 +2,6 @@ const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
     host: process.env.DATABASE_HOST,
-    dialect: process.env.DATABASE_DIALECT
+    dialect: process.env.DATABASE_DIALECT,
+    port: process.env.DATABASE_PORT
 });
