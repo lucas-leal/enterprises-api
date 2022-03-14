87cc0143db7eefeceed79bbc0e7eb6e42b07642b
Lucas Leal <lucas.ity.leal@gmail.com>
2022-03-13 23:23:49 -0300

    refactor: adjust code

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -18,29 +18,30 @@ router.post(
     scopes('employees.create'),
     validation.required(['name', 'documentNumber', 'birthDate', 'position', 'startDate', 'salary', 'address']),
     async (req, res, next) => {
-    try {
-        const employee = await Employee.create({
-            name: req.body.name,
-            documentNumber: req.body.documentNumber,
-            birthDate: req.body.birthDate,
-            position: req.body.position,
-            startDate: req.body.startDate,
-            salary: req.body.salary
-        });
-    
-        const address = await Address.create({
-            addressLine: 'foo',
-            city: 'Belo Horizonte',
-            state: 'MG',
-            zipCode: '30890000',
-            country: 'Brazil',
-            employeeId: employee.id
-        });
-    
-        res.send(employee);
-    } catch (error) {
-        next(error);
+        try {
+            const employee = await Employee.create({
+                name: req.body.name,
+                documentNumber: req.body.documentNumber,
+                birthDate: req.body.birthDate,
+                position: req.body.position,
+                startDate: req.body.startDate,
+                salary: req.body.salary
+            });
+        
+            const address = await Address.create({
+                addressLine: 'foo',
+                city: 'Belo Horizonte',
+                state: 'MG',
+                zipCode: '30890000',
+                country: 'Brazil',
+                employeeId: employee.id
+            });
+        
+            res.send(employee);
+        } catch (error) {
+            next(error);
+        }
     }
-});
+);
 
 module.exports = router;
