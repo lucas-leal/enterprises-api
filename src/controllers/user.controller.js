5b53f9cfedc6f0c567c604c816e6d2e549524522
Lucas Leal <lucas.ity.leal@gmail.com>
2022-03-13 23:06:59 -0300

    feat: add validation on user endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 const scopes = require('../middlewares/scopes.middleware');
+const validation = require('../middlewares/validation.middleware');
 
 const router = express.Router();
 
@@ -19,26 +20,31 @@ router.get('/', scopes('users.list'), async (req, res) => {
     res.send(users);
 });
 
-router.post('/', scopes('users.create'), async (req, res, next) => {
-    try {
-        let salt = Math.random();
-        let hash = bcrypt.hashSync(req.body.password, salt)
-        
-        const user = await User.create({
-            username: req.body.username,
-            password: hash,
-            scopes: req.body.scopes,
-            employeeId: req.body.employeeId
-        });
+router.post(
+    '/',
+    scopes('users.create'),
+    validation.required(['username', 'password', 'scopes', 'employeeId']),
+    async (req, res, next) => {
+        try {
+            let salt = Math.random();
+            let hash = bcrypt.hashSync(req.body.password, salt)
+            
+            const user = await User.create({
+                username: req.body.username,
+                password: hash,
+                scopes: req.body.scopes,
+                employeeId: req.body.employeeId
+            });
 
-        res.send({
-            id: user.id, 
-            username: user.username
-        });
-    } catch (error) {
-        next(error);
+            res.send({
+                id: user.id, 
+                username: user.username
+            });
+        } catch (error) {
+            next(error);
+        }
     }
-});
+);
 
 module.exports = router;
 
